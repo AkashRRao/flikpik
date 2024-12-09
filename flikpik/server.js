24ece2f04adb9e00762aabb16a6d3e48fa891200
Akash Rao <33335296+AkashRRao@users.noreply.github.com>
2024-12-09 23:33:30 +0000

    Add basic logic to join a room with a random name.

--- flikpik/server.js
+++ flikpik/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 const path = require('path');
-const app = express();
-const port = 3000;
 
-const movies = [
-    { title: 'Movie Title 1', poster: 'movie1.jpg' },
-    { title: 'Movie Title 2', poster: 'movie2.jpg' },
-    { title: 'Movie Title 3', poster: 'movie3.jpg' }
-];
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const port = 3000;
 
 // Serve static files (e.g., images, CSS, JS)
 app.use(express.static(path.join(__dirname)));
@@ -17,12 +16,39 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/movie-of-the-night', (req, res) => {
-    const randomIndex = Math.floor(Math.random() * movies.length);
-    const movie = movies[randomIndex];
-    res.json(movie);
+// Random name generator function
+function generateRandomName() {
+    const adjectives = ['Quick', 'Lazy', 'Happy', 'Sad', 'Angry'];
+    const animals = ['Fox', 'Dog', 'Cat', 'Mouse', 'Bear'];
+    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const animal = animals[Math.floor(Math.random() * animals.length)];
+    return `${adjective}${animal}${Math.floor(Math.random() * 1000)}`;
+}
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    const userName = generateRandomName();
+    socket.emit('userAssigned', userName);
+
+    socket.on('createRoom', () => {
+        const roomName = Math.random().toString(36).substring(2, 7);
+        socket.join(roomName);
+        socket.emit('roomCreated', roomName);
+        console.log(`Room created: ${roomName}`);
+    });
+
+    socket.on('joinRoom', (roomName) => {
+        socket.join(roomName);
+        socket.emit('roomJoined', roomName);
+        console.log(`User joined room: ${roomName}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
