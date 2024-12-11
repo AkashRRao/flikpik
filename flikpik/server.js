6e3d00c75ef6f4e8a1ca2306a48354b4bc4d4c44
Rahul Joseph Fernandez <49276090+rahuljosephfernandez@users.noreply.github.com>
2024-12-11 21:09:07 +0000

    edits

--- flikpik/server.js
+++ flikpik/server.js
@@ -16,18 +16,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const movies = [
-    {
-        title: 'The Shawshank Redemption',
-        // png for the poster
-        poster: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg',
-    },
-    {
-        title: "Schindler's List",
-        poster: 'https://upload.wikimedia.org/wikipedia/en/3/38/Schindler%27s_List_movie.jpg',
-    }
-]
-
 // Random name generator function
 function generateRandomName() {
     const adjectives = ['Quick', 'Lazy', 'Happy', 'Sad', 'Angry'];
@@ -37,23 +25,25 @@ function generateRandomName() {
     return `${adjective}${animal}${Math.floor(Math.random() * 1000)}`;
 }
 
-const activeRooms = new Set();
+// List of active rooms
+const activeRooms = new Map();
 
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log('a user connected');
 
     const userName = generateRandomName();
     socket.emit('userAssigned', userName);
 
     socket.on('createRoom', () => {
         const roomName = Math.random().toString(36).substring(2, 7);
-        activeRooms.add(roomName);
+        activeRooms.set(roomName, [userName]);
         socket.join(roomName);
         socket.emit('roomCreated', roomName);
     });
 
     socket.on('joinRoom', (roomName) => {
         if (activeRooms.has(roomName)) {
+            activeRooms.get(roomName).push(userName);
             socket.join(roomName);
             socket.emit('roomJoined', roomName);
         } else {
@@ -61,18 +51,18 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('enterMovieSelection', () => {
-        const roomName = Array.from(socket.rooms)[1]; // Get the room name
-        if (roomName) {
-            io.to(roomName).emit('enterMovieSelection', movies);
-        }
+    socket.on('submitPreferences', (preferences) => {
+        const roomName = Array.from(socket.rooms)[1]; // Get the room name the user is in
+        console.log(`Preferences submitted for room ${roomName}:`, preferences);
+        const users = activeRooms.get(roomName);
+        io.to(roomName).emit('showMovieSelection', users);
     });
 
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        console.log('a user disconnected');
     });
 });
 
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`server is running on http://localhost:${port}`);
 });
